8184f8f212e84c4a16ae83a0a351b2679e7cf6f7
Mangeshi <thisismangeshi@gmail.com>
2021-11-13 00:55:12 +0530

    added styling

--- src/Components/Pokemon.jsx
+++ src/Components/Pokemon.jsx
@@ -27,10 +27,8 @@ const Pokemon = () => {
     const { id } = pokemonid;
     const pokemonUrl = `https://pokeapi.co/api/v2/pokemon/${id}/`;
 
-
     // pokemon species url
     const pokemonSpeciesUrl = `https://pokeapi.co/api/v2/pokemon-species/${id}/`;
-
 
     // Fetching the data from Api
     const PokemonDetails = async () => {
@@ -72,14 +70,71 @@ const Pokemon = () => {
     const poke_weight_lbs = (weight * 0.22046).toFixed(1);
 
 
+    const SelectColor = (name) => {
+        switch (name) {
+            case "normal":
+                return "#A8A77A";
+            case "fire":
+                return "#ee8130";
+            case "water":
+                return "#6390f0";
+            case "electric":
+                return "#f7d02c";
+            case "grass":
+                return "#7ac74c";
+            case "ice":
+                return "#96d9d6";
+            case "fighting":
+                return "#c22e28";
+            case "poison":
+                return "#a33ea1";
+            case "ground":
+                return "#e2bf65";
+            case "flying":
+                return "#a98ff3";
+            case "psychic":
+                return "#f95587";
+            case "bug":
+                return "#a6b91a";
+            case "rock":
+                return "#b6a136";
+            case "ghost":
+                return "#735797";
+            case "dragon":
+                return "#6f35fc";
+            case "dark":
+                return "#705746";
+            case "steel":
+                return "#b7b7ce";
+            case "fairy":
+                return "#d685ad";
+            default:
+                return undefined;
+        }
+    };
+
+
+    const changeBackgroundColor = () => {
+        return (
+                typeof types !== "undefined" ? (
+                    types.map((info) => {
+                        const { type } = info;
+                        const { name } = type;
+
+                        return name
+                    })
+                ) : (
+                    <p></p>
+                )
+        );
+    };
+    const bg=changeBackgroundColor()
+
     return (
         <>
-            <div className="pokemon-container">
+            <div className="pokemon-container" >
                 <div className="cont">
-                    <div
-                        className="image-cont"
-
-                    >
+                    <div className="image-cont" style={{backgroundColor: `${SelectColor(bg[0])}`}}>
                         <div className="back-btn">
                             <BackButton />
                         </div>
